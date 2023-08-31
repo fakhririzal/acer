a29bae86b61cbd36647ab8b7b93c68a7ff51023a
fakhririzal <143717960+fakhririzal@users.noreply.github.com>
2023-08-31 20:10:52 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// //togle clas active 
+// const navbarNav = document.querySelector('.navbar-nav')
+
+// document.querySelector('#humberger-menu').
+// onclick = () => {
+//     navbarNav.classList.toggle('active');
+// };
+
+// // click luar
+
+// const humberger = document.querySelector
+// ('#humberger-menu');
+
+// document.addEventListener('click', function(e) {
+//     if(!humberger.contains(e.target) && !navbarNav.contains(e.target)) {
+//         navbarNav.classList.remove('active');
+//     }
+// });   
